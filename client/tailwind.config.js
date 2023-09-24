ffe13b7888327b46425f9ed8312ee8c4f03b5fe9
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-23 20:56:42 -0400

    added logo and a map photo. as well as setting colors in tailwind config for red and green titled sgreen and sred.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    //color accents
+    colors: {
+      sred: "#ce2b37", //stasios red
+      sgreen: "#019246", //stasios green
+    },
     extend: {},
   },
   plugins: [],
