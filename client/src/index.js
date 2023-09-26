6af8da1cc3e32fd403edc87c70a0bd2579591b18
Matthew Hurst <121309086+treesaretall@users.noreply.github.com>
2023-09-25 21:09:00 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
-import App from './App';
->>>>>>> e91067e9743728d71c96eecd8ba9fd43829946cf
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
