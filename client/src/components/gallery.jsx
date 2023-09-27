753c710da806eacdc84d7d2150c1655ae19bea87
treesaretall <himatthewhurst@hotmail.com>
2023-09-27 19:42:57 -0400

    fixed img zoom finished contact page

--- client/src/components/gallery.jsx
+++ client/src/components/gallery.jsx
@@ -7,22 +7,19 @@ class Gallery extends React.Component {
   render() {
     return (
       <div name="gallery">
-      <ResponsiveMasonry
-        
-        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
-      >
-        <Masonry>
-          {galleryImages.map((image, i) => (
-            <img
-              key={i}
-              src={image.image}
-              style={{ width: "100%", display: "block" }}
-              alt={image.title}
-              className="hover:scale-110 hover:opacity-80 duration-700"
-            />
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry>
+            {galleryImages.map((image, i) => (
+              <img
+                key={i}
+                src={image.image}
+                style={{ width: "100%", display: "block" }}
+                alt={image.title}
+                className=" hover:opacity-70 duration-700"
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
     );
   }
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,17 +1,33 @@
 import ContactForm from "../components/contactForm";
-import { mapImage } from "../constants";
+import map from "../assets/othermap.jpg";
 
 import { FaFacebookF, FaYelp, FaInstagram } from "react-icons/fa";
 
 const Contact = () => {
+  const fbNav = () => {
+    window.open(
+      "https://www.facebook.com/p/Stasios-Italian-deli-100063550872193/",
+      "_blank"
+    );
+  };
+  const igNav = () => {
+    window.open("https://www.instagram.com/stasiositaliandeli/", "_blank");
+  };
+  const yelpNav = () => {
+    window.open("https://www.yelp.com/biz/stasios-orlando", "_blank");
+  };
+
+  const googleMapsUrl =
+    "https://www.google.com/maps/place/Stasio's+Italian+Deli+%26+Market/@28.5457224,-81.354684,17z/data=!4m6!3m5!1s0x88e77ada611aaaab:0xb80ed6b6c26907dd!8m2!3d28.5457224!4d-81.3521037!16s%2Fg%2F11ggsrj9kd?entry=ttu";
+
   return (
     <>
       <div className="flex m-auto pt-16 justify-center items-center">
         <div className="flex w-[500px] bg-bgMap bg-center h-16 justify-center items-center rounded-full">
-          <h1 className="text-4xl font-bold text-left">
-            <span className="text-[#019246] drop-shadow-2xl">CO</span>
-            <span className="text-white drop-shadow-2xl">NTA</span>
-            <span className="text-[#CE2B37] drop-shadow-2xl">CT</span>
+          <h1 className="font-bold text-[3.4rem] text-left">
+            <span className="text-white drop-shadow-2xl bg-[#019246]">CO</span>
+            <span className="text-black drop-shadow-2xl bg-white">NTA</span>
+            <span className="text-white drop-shadow-2xl bg-[#CE2B37]">CT</span>
           </h1>
         </div>
       </div>
@@ -21,7 +37,7 @@ const Contact = () => {
           <div className="grid grid-cols-2 items-center">
             <p className="">
               <span className="font-bold">Phone:</span>
-              <br></br> (000)000-0000
+              <br></br> (407) 277-7755
             </p>
             <p className="">
               <span className="font-bold">Email:</span>
@@ -31,40 +47,65 @@ const Contact = () => {
               <p className="">
                 <span className="font-bold">Physical Address:</span>
                 <br></br>
-                Stacios Rd, <br></br>Orlando Fl 32948
+                2320 E Robinson St, <br></br>Orlando, FL 32803
               </p>
             </div>
             <div className="pt-6">
               <p className="">
                 <span className="font-bold">Mailing Address:</span>
                 <br></br>
-                Stacios Rd, <br></br>Orlando Fl 32948
+                2320 E Robinson St, <br></br>Orlando, FL 32803
               </p>
             </div>
             <div className="flex col-span-2 items-center justify-center py-6 space-x-8">
               <FaFacebookF
-                onClick=""
+                onClick={fbNav}
                 className="cursor-pointer hover:scale-110 ease-in duration-200"
                 size={30}
               />
               <FaInstagram
-                onClick=""
+                onClick={igNav}
                 className="cursor-pointer hover:scale-110 ease-in duration-200 pl"
                 size={30}
               />
               <FaYelp
-                onClick=""
+                onClick={yelpNav}
                 className="cursor-pointer hover:scale-110 ease-in duration-200"
                 size={30}
               />
             </div>
           </div>
+          <div className="relative">
+            {/* Dark overlay with text */}
+            <a
+              href={googleMapsUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="absolute inset-0 flex items-center justify-center bg-lightgray opacity-75 hover:bg-black transition-opacity duration-300">
+              <div className="text-white text-center opacity-0 transition-opacity duration-300">
+                <p className="text-5xl">Come and visit</p>
+              </div>
+            </a>
 
-          <img
-            className="max-h-max"
-            src={mapImage.image}
-            alt={mapImage.description}
-          />
+            {/* Hover effect for text */}
+            <style>
+              {`
+          .bg-lightgray {
+            transition: background-color 0.3s;
+          }
+
+          .bg-lightgray:hover .opacity-0 {
+            opacity: 1;
+          }
+        `}
+            </style>
+
+            <img
+              src={map}
+              alt="map"
+              className="w-full h-[300px] object-cover hover:scale-110"
+            />
+          </div>
         </div>
         <div className="flex items-center justify-center py-16">
           <ContactForm />
