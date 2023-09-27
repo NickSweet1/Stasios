91f7db0dd3fb6dd254530b343680d25b404bca7f
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-09-27 16:43:39 -0400

    Navbar now responds to height and adjusts to top when scrolled

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-scroll';
 import Logo from '../../assets/LogoStasios.png';
 import BannerMap from '../../assets/BannerMap.jpg';
 
-// Define an array of navigation links
 const links = [
   {
     id: 1,
@@ -25,37 +24,60 @@ const links = [
 ];
 
 const NavBar = () => {
-  // State to manage the visibility of the mobile navigation menu
   const [nav, setNav] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div
-      className='relative w-full h-56'
+      className={`relative w-full h-56`}
       style={{
         backgroundImage: `url(${BannerMap})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
     >
-      {/* Text elements */}
-      <p className='absolute text-white text-[1.5rem] top-[2.3rem] inset-0 flex justify-center text-shadow-NavBar'>Pickup or Dine-In! 407-277-7755</p>
-      <p className='absolute text-white text-[1rem] top-[6.8rem] inset-0 flex justify-center items-center text-shadow-NavBar'>Stasio's Italian Deli & Market</p>
-      <p className='absolute text-white text-[1.2rem] top-[10rem] inset-0 flex justify-center items-center text-shadow-NavBar'>2320 E Robinson St, Orlando, FL 32803</p>
+      <p className='absolute text-white text-[1.5rem] top-[2.3rem] inset-0 flex justify-center text-shadow-NavBar'>
+        Pickup or Dine-In! 407-277-7755
+      </p>
+      <p className='absolute text-white text-[1rem] top-[6.8rem] inset-0 flex justify-center items-center text-shadow-NavBar'>
+        Stasio's Italian Deli & Market
+      </p>
+      <p className='absolute text-white text-[1.2rem] top-[10rem] inset-0 flex justify-center items-center text-shadow-NavBar'>
+        2320 E Robinson St, Orlando, FL 32803
+      </p>
       
-      {/* White Banner Background */}
-      <div className='fixed w-full h-20 top-[7rem] transform -translate-y-1/2 bg-white z-10 shadow-2xl opacity-90'></div>
-      
-      {/* Navigation elements */}
-      <div className='flex justify-between items-center fixed w-full h-56 z-20'>
+      <div className={`fixed w-full h-20 bg-white z-10 shadow-2xl opacity-90 ${
+        scrolling ? 'top-[0rem]' : 'top-[4.5rem]'
+      } transition-top duration-[400ms] ease-in-out`}>
+      </div>
+      <div className={`flex justify-between items-center fixed w-full h-56 z-20 ${
+        scrolling ? 'top-[-4.5rem]' : 'top-[0rem]'
+      } transition-top duration-[400ms] ease-in-out`}>
+
         <div>
           <img
             src={Logo}
-            alt=''
-            className='w-56 ml-20'
+            alt='logo'
+            className='w-56 ml-20 opacity-100'
           />
         </div>
         
-        {/* Desktop navigation links */}
         <ul className='hidden md:flex'>
           {links.map(({ id, link }) => (
             <li
@@ -69,22 +91,20 @@ const NavBar = () => {
           ))}
         </ul>
 
-        {/* Mobile menu toggle */}
         <div
           onClick={() => setNav(!nav)}
-          className='cursor-pointer pr-4 z-10 md:hidden text-red-950'
+          className='cursor-pointer pr-4 md:hidden text-red-950'
         >
           {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
         </div>
       </div>
 
-      {/* Mobile navigation menu */}
       {nav && (
-        <ul className='flex flex-col justify-center items-center absolute top-0 w-full h-screen bg-zinc-50 z-10 '>
+        <ul className='flex flex-col justify-center items-center fixed top-0 w-full h-screen bg-zinc-50 z-10 pt-[6rem]'>
           {links.map(({ id, link }) => (
             <li
               key={id}
-              className='px-4 cursor-pointer capitalize py-[1.2rem] text-4xl hover:scale-110 duration-200 text-red-950'
+              className='px-4 cursor-pointer capitalize py-[2rem] text-4xl hover:scale-110 duration-200 text-red-950'
             >
               <Link
                 onClick={() => setNav(!nav)}
@@ -103,4 +123,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
