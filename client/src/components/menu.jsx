1e17f76b4a9713678d3849192b84309714299857
Joshua Vaneps <111782130+JoshuaVaneps@users.noreply.github.com>
2023-09-27 08:15:06 -0400

    Revert "added utils folder with query and mutations files. menu component now…"

--- client/src/components/menu.jsx
+++ client/src/components/menu.jsx
@@ -1,70 +1,14 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_SUBS } from '../utils/queries';
 
 const Menu = () => {
-  // Use the useQuery hook to fetch data and handle errors
-  const { data, loading, error } = useQuery(QUERY_SUBS);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  // Handle GraphQL query errors
-  if (error) {
-    console.error('GraphQL Error:', error);
-    return (
-      <div className="error-message">
-        <p>Error: {error.message}</p>
-      </div>
-    );
-  }
-
-  // Assuming your GraphQL query returns an array of menu items
-  const menuItems = data.subs;
-
-  const midpoint = Math.ceil(menuItems.length / 2);
-
-  // Split the menuItems array into two separate arrays
-  const firstHalf = menuItems.slice(0, midpoint);
-  const secondHalf = menuItems.slice(midpoint);
-
-  const renderFirstMenuItems = () => {
-    return firstHalf.map((item) => (
-      <li key={item._id} className='mb-4'>
-        <div className='bg-gray-100 rounded-lg p-4'>
-          <strong className='text-lg md:text-xl lg:text-2xl'>{item.subName}</strong>
-          <p className='mt-2'>
-            {item.ingredients}<br />
-            <em className='italic'>{item.price}</em>
-          </p>
-        </div>
-      </li>
-    ));
-  };
-
-  const renderSecondMenuItems = () => {
-    return secondHalf.map((item) => (
-      <li key={item._id} className='mb-4'>
-        <div className='bg-gray-100 rounded-lg p-4'>
-          <strong className='text-lg md:text-xl lg:text-2xl'>{item.subName}</strong>
-          <p className='mt-2'>
-            {item.ingredients}<br />
-            <em className='italic'>{item.price}</em>
-          </p>
-        </div>
-      </li>
-    ));
-  };
-
   return (
     <div name='menu' className='flex flex-col items-center min-h-screen max-h-3/4 relative px-4 max-w-[900px] mx-auto'>
       {/* Add a small top border */}
       <div className='w-full md:w-px bg-gray-300 h-1 mt-4'></div>
       <div className='text-3xl md:text-4xl lg:text-5xl text-center mb-6 relative'>
-        <span className='border-b-4 border-sgreen w-1/3 inline-block'></span>
+        <span className='border-b-4 border-green-500 w-1/3 inline-block'></span>
         <span className='border-b-4 border-white w-1/3 inline-block'></span>
-        <span className='border-b-4 border-sred w-1/3 inline-block'></span>
+        <span className='border-b-4 border-red-500 w-1/3 inline-block'></span>
         <span className='relative z-10 bg-white px-2 md:px-4'>Our Menu</span>
         <br />
         <span className="text-sm text-gray-400">Sandwiches / Desserts / Delicacies</span> {/* Smaller and light grey */}
@@ -72,14 +16,84 @@ const Menu = () => {
       <div className='flex flex-col md:flex-row w-full'>
         <div className='w-full md:w-1/2 p-6'>
           <ul className='text-gray-600'>
-            {renderFirstMenuItems()}
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Prosciutto Panini</strong>
+                <p className='mt-2'>
+                  A classic Italian sandwich with thinly sliced prosciutto, fresh mozzarella, and arugula. <br />
+                  <em className='italic'>$9.99</em>
+                </p>
+              </div>
+            </li>
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Caprese Salad</strong>
+                <p className='mt-2'>
+                  A refreshing salad made with tomatoes, fresh mozzarella, basil, and balsamic glaze. <br />
+                  <em className='italic'>$8.49</em>
+                </p>
+              </div>
+            </li>
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Lasagna</strong>
+                <p className='mt-2'>
+                  Layers of pasta, rich meat sauce, creamy béchamel, and melted cheese. <br />
+                  <em className='italic'>$12.99</em>
+                </p>
+              </div>
+            </li>
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Margherita Pizza</strong>
+                <p className='mt-2'>
+                  A classic pizza with tomato sauce, fresh mozzarella, basil, and olive oil. <br />
+                  <em className='italic'>$10.99</em>
+                </p>
+              </div>
+            </li>
             {/* Add more menu items here */}
           </ul>
         </div>
         <div className='w-full md:w-px bg-gray-300 md:h-[80vh]'></div>
         <div className='w-full md:w-1/2 p-6'>
           <ul className='text-gray-600'>
-            {renderSecondMenuItems()}
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Tiramisu</strong>
+                <p className='mt-2'>
+                  An Italian dessert made with layers of coffee-soaked ladyfingers and mascarpone cheese. <br />
+                  <em className='italic'>$6.99</em>
+                </p>
+              </div>
+            </li>
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Ravioli al Pomodoro</strong>
+                <p className='mt-2'>
+                  Homemade ravioli stuffed with ricotta cheese, served in a tomato and basil sauce. <br />
+                  <em className='italic'>$11.49</em>
+                </p>
+              </div>
+            </li>
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Chicken Alfredo</strong>
+                <p className='mt-2'>
+                  Fettuccine pasta with creamy Alfredo sauce and grilled chicken. <br />
+                  <em className='italic'>$13.99</em>
+                </p>
+              </div>
+            </li>
+            <li className='mb-4'>
+              <div className='bg-gray-100 rounded-lg p-4'>
+                <strong className='text-lg md:text-xl lg:text-2xl'>Minestrone Soup</strong>
+                <p className='mt-2'>
+                  A hearty Italian vegetable soup with pasta and beans. <br />
+                  <em className='italic'>$6.49</em>
+                </p>
+              </div>
+            </li>
             {/* Add more menu items here */}
           </ul>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri : 'http://localhost:3001/graphql',
-  cache: new InMemoryCache() ,
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ApolloProvider client={client}>
+  <React.StrictMode>
     <App />
-  </ApolloProvider>
+  </React.StrictMode>
 );
