2ab12aafba9322bb69e8ffd2716fec1418fc9734
Nick <nicksweetj@gmail.com>
2023-09-28 17:50:06 -0400

    added seeds for coffee and desserts

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const Sub = require('./sub');
 const User = require('./user');
 const Contact = require('./contact');
+const Coffee = require('./coffee');
+const Desserts = require('./desserts');
 
-module.exports = { Sub, User, Contact }
+module.exports = { Sub, User, Contact, Coffee, Desserts }
