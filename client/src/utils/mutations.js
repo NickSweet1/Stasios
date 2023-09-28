0d95a84e59f4e98fef42c2301d86795986491881
treesaretall <himatthewhurst@hotmail.com>
2023-09-28 07:44:44 -0400

    got the delete menu item to work

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const DELETE_MENU_ITEM = gql`
   mutation removeSub($_id: ID!) {
-    removeSub(id: $_id) {
+    removeSub(_id: $_id) {
       _id
     }
   }
