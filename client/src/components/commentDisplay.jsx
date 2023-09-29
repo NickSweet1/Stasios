74b11052f0728b9542e3244a9a33c7e646355695
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-09-29 11:24:20 -0400

    Added styles to the dashboard comments area

--- client/src/components/commentDisplay.jsx
+++ client/src/components/commentDisplay.jsx
@@ -14,11 +14,11 @@ const MessageCardComponent = () => {
   }
 
   return (
-    <div className="message-card-container">
-      <h1>Messages</h1>
+    <div className="flex flex-col justify-center message-card-container drop-shadow-2xl">
+      <h1 className="text-center text-2xl text-amber-950 bg-white mb-5 text-shadow-css rounded-lg">Messages</h1>
       <div className="message-card-scroll">
         {data.contacts.map((contact) => (
-          <div key={contact._id} className="message-card bg-white">
+          <div key={contact._id} className="message-card bg-white text-amber-800 text-shadow-css">
             <h3>{contact.name}</h3>
             <p>Email: {contact.email}</p>
             <p>Message: {contact.message}</p>
