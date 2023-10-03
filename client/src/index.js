82a8a87e7565b78bb3a803c1faa5809424a61910
Joshua Vaneps <joshuavaneps@yahoo.com>
2023-10-02 21:16:24 -0400

    uri change

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri : 'http://localhost:3001/graphql',
-  cache: new InMemoryCache() ,
+  uri: "/graphql",
+  cache: new InMemoryCache(),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
