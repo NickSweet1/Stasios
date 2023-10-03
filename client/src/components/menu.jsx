be59dad3079592b1853c1e30acadf1e254789e44
William Tikhonenko <williamtikhonenko@gmail.com>
2023-10-02 20:44:02 -0400

    conditionals to render different menus is now up and running everything is pretty

--- client/src/components/menu.jsx
+++ client/src/components/menu.jsx
@@ -1,95 +1,81 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_SUBS } from '../utils/queries';
+import React, { useState } from "react";
+
+//imoprting the new menus for the sick conditional/tertiary
+import CoffeeMenu from "./coffeeMenu";
+import DessertMenu from "./dessertMenu";
+import SandwichMenu from "./sandwhichMenu";
 
 const Menu = () => {
-  // Use the useQuery hook to fetch data and handle errors
-  const { data, loading, error } = useQuery(QUERY_SUBS);
+  const [showCoffeeMenu, setShowCoffeeMenu] = useState(false);
+  const [showDessertMenu, setShowDessertMenu] = useState(false);
+  const [showSandwichMenu, setShowSandwichMenu] = useState(true);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  // Handle GraphQL query errors
-  if (error) {
-    console.error('GraphQL Error:', error);
-    return (
-      <div className="error-message">
-        <p>Error: {error.message}</p>
-      </div>
-    );
-  }
-
-  // Assuming your GraphQL query returns an array of menu items
-  const menuItems = data.subs;
-
-  const midpoint = Math.ceil(menuItems.length / 2);
-
-  // Split the menuItems array into two separate arrays
-  const firstHalf = menuItems.slice(0, midpoint);
-  const secondHalf = menuItems.slice(midpoint);
-
-  const renderFirstMenuItems = () => {
-    return firstHalf.map((item) => (
-      <li key={item._id} className='mb-4'>
-        <div className='bg-gray-100 rounded-lg p-4 text-amber-800'>
-          <strong className='text-lg md:text-xl lg:text-2xl text-amber-950'>{item.subName}</strong>
-          <p className='mt-2'>
-            {item.ingredients}<br />
-            <em className='italic'>${parseFloat(item.price).toFixed(2)}</em>
-          </p>
-        </div>
-      </li>
-    ));
+  const showSandwichMenuHandler = () => {
+    setShowCoffeeMenu(false);
+    setShowDessertMenu(false);
+    setShowSandwichMenu(true);
   };
 
-  const renderSecondMenuItems = () => {
-    return secondHalf.map((item) => (
-      <li key={item._id} className='mb-4'>
-        <div className='bg-gray-100 rounded-lg p-4'>
-          <strong className='text-lg md:text-xl lg:text-2xl text-amber-950'>{item.subName}</strong>
-          <p className='mt-2'>
-            {item.ingredients}<br />
-            <em className='italic'>${parseFloat(item.price).toFixed(2)}</em>
-          </p>
-        </div>
-      </li>
-    ));
+  const showDessertMenuHandler = () => {
+    setShowCoffeeMenu(false);
+    setShowDessertMenu(true);
+    setShowSandwichMenu(false);
+  };
+
+  const showCoffeeMenuHandler = () => {
+    setShowCoffeeMenu(true);
+    setShowDessertMenu(false);
+    setShowSandwichMenu(false);
   };
 
   return (
-    <div name='menu' className='bg-white rounded-xl items-center min-h-screen max-h-3/4 relative mb-4 p-4 mt-[13rem] px-4 max-w-[1500px] mx-auto text-amber-950'>
+    <div
+      name="menu"
+      className="bg-white rounded-xl items-center min-h-screen max-h-3/4 relative mb-4 p-4 mt-[13rem] px-4 max-w-[1500px] mx-auto text-amber-950"
+    >
       {/* Add a small top border */}
-      <div className='w-full md:w-px bg-gray-300 h-1 mt-4'></div>
-      <div className='text-3xl md:text-5xl lg:text-6xl text-center mb-6 relative'>
-        <span className='border-b-4 border-sgreen w-1/3 inline-block'></span>
-        <span className='border-b-4 border-white w-1/3 inline-block'></span>
-        <span className='border-b-4 border-sred w-1/3 inline-block'></span>
-        <span className='relative z-1 px-2 md:px-4'>Our Menu</span>
+      <div className="w-full md:w-px bg-gray-300 h-1 mt-4"></div>
+      <div className="text-3xl md:text-5xl lg:text-6xl text-center mb-6 relative">
+        <span className="border-b-4 border-sgreen w-1/3 inline-block"></span>
+        <span className="border-b-4 border-white w-1/3 inline-block"></span>
+        <span className="border-b-4 border-sred w-1/3 inline-block"></span>
+        <span className="relative z-1 px-2 md:px-4">Our Menu</span>
         <br />
-        <span className="text-[30px] text-amber-800 opacity-50"><span className='underline decoration-black cursor-pointer'>Sandwiches</span> / <span className="cursor-pointer">Desserts</span> / <span className="cursor-pointer">Coffees</span></span> {/* Smaller and light grey */}
+        <span className="text-[30px] text-amber-800 opacity-50">
+          <span
+            className={`border-b-4 cursor-pointer ${showSandwichMenu ? "border-sgreen" : ""}`}
+            onClick={showSandwichMenuHandler}
+          >
+            Sandwiches
+          </span>
+          /
+          <span
+            className={`border-b-4 cursor-pointer ${showDessertMenu ? "border-white" : ""}`}
+            onClick={showDessertMenuHandler}
+          >
+            Desserts
+          </span>
+          /
+          <span
+            className={`border-b-4 cursor-pointer ${showCoffeeMenu ? "border-sred" : ""}`}
+            onClick={showCoffeeMenuHandler}
+          >
+            Coffees
+          </span>
+        </span>{" "}
+        {/* Smaller and light grey */}
       </div>
-      <div className='flex flex-col md:flex-row w-full'>
-        <div className='w-full md:w-[750px] p-10'>
-          <div className='flex flex-col items-center'>
-            <ul className='text-gray-600'>
-              {renderFirstMenuItems()}
-              {/* Add more menu items here */}
-            </ul>
-          </div>
-        </div>
-        <div className='w-full md:w-px bg-gray-300 md:h-[80vh]'></div>
-        <div className='w-full md:w-[750px] p-10'>
-          <div className='flex flex-col items-center'>
-            <ul className='text-amber-800'>
-              {renderSecondMenuItems()}
-              {/* Add more menu items here */}
-            </ul>
-          </div>
-        </div>
-      </div>
-      {/* Add a small bottom border */}
-      <div className='w-full md:w-px bg-gray-300 h-1'></div>
+      {showSandwichMenu && (
+  <SandwichMenu/>
+)}
+{showDessertMenu && (
+  <DessertMenu/>
+)}
+{showCoffeeMenu && (
+  <CoffeeMenu/>
+)}
+
+      <div className="w-full md:w-px bg-gray-300 h-1"></div>
     </div>
   );
 };
--- client/src/components/sandwhichMenu.jsx
+++ client/src/components/sandwhichMenu.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_DESSERT } from '../utils/queries';
+import { QUERY_SUBS } from '../utils/queries';
 
-const DessertMenu = () => {
-  // Use the useQuery hook to fetch data and handle errors
-  const { data, loading, error } = useQuery(QUERY_DESSERT);
+const SandwichMenu = () => {
+  const { data, loading, error } = useQuery(QUERY_SUBS);
 
   if (loading) {
     return <p>Loading...</p>;
   }
 
-  // Handle GraphQL query errors
   if (error) {
     console.error('GraphQL Error:', error);
     return (
@@ -20,8 +18,7 @@ const DessertMenu = () => {
     );
   }
 
-  // Assuming your GraphQL query returns an array of menu items
-  const menuItems = data.desserts;
+  const menuItems = data.subs;
 
   const midpoint = Math.ceil(menuItems.length / 2);
 
@@ -33,9 +30,9 @@ const DessertMenu = () => {
     return firstHalf.map((item) => (
       <li key={item._id} className='mb-4'>
         <div className='bg-gray-100 rounded-lg p-4 text-amber-800'>
-          <strong className='text-lg md:text-xl lg:text-2xl text-amber-950'>{item.name}</strong>
+          <strong className='text-lg md:text-xl lg:text-2xl text-amber-950'>{item.subName}</strong>
           <p className='mt-2'>
-            {item.description}<br />
+            {item.ingredients}<br />
             <em className='italic'>${parseFloat(item.price).toFixed(2)}</em>
           </p>
         </div>
@@ -47,9 +44,9 @@ const DessertMenu = () => {
     return secondHalf.map((item) => (
       <li key={item._id} className='mb-4'>
         <div className='bg-gray-100 rounded-lg p-4'>
-          <strong className='text-lg md:text-xl lg:text-2xl text-amber-950'>{item.name}</strong>
+          <strong className='text-lg md:text-xl lg:text-2xl text-amber-950'>{item.subName}</strong>
           <p className='mt-2'>
-            {item.description}<br />
+            {item.ingredients}<br />
             <em className='italic'>${parseFloat(item.price).toFixed(2)}</em>
           </p>
         </div>
@@ -58,26 +55,21 @@ const DessertMenu = () => {
   };
 
   return (
-      <div className='flex flex-col md:flex-row w-full'>
+    <div className='flex flex-col md:flex-row w-full'>
         <div className='w-full md:w-[750px] p-10'>
-          <div className='flex flex-col items-center'>
-            <ul className='text-gray-600'>
-              {renderFirstMenuItems()}
-              {/* Add more menu items here */}
-            </ul>
-          </div>
+        <ul className='text-amber-800'>
+            {renderFirstMenuItems()}
+        </ul>
         </div>
         <div className='w-full md:w-px bg-gray-300 md:h-[80vh]'></div>
         <div className='w-full md:w-[750px] p-10'>
           <div className='flex flex-col items-center'>
             <ul className='text-amber-800'>
-              {renderSecondMenuItems()}
-              {/* Add more menu items here */}
+            {renderSecondMenuItems()}
             </ul>
           </div>
         </div>
       </div>
-  );
-};
+  )};
 
-export default DessertMenu;
+export default SandwichMenu;
